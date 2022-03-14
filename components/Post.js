1590251753c35ca5f7de8abad1b54a8d3bcd3e96
yamada <shinya.yamada@benext-corp.com>
2022-03-15 08:15:51 +0900

    ✨ Create individual blog page

--- components/Post.js
+++ components/Post.js
@@ -1,12 +1,16 @@
+import Link from "next/link";
+
 export const Post = ( {post} ) => {
     return (
         <div>
             <span>{post.id}</span>
             {" : "}
-            <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
-                {post.title}
-            </span>
+            <Link href={`/posts/${post.id}`}>
+                <span className="cursor-pointer text-blue-500 border-b border-blue-500 hover:bg-gray-200">
+                    {post.title}
+                </span>
+            </Link>
         </div>
-    )
+    );
 }
 export default Post;
--- pages/blog-page.js
+++ pages/blog-page.js
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 import { getAllPostsData } from "../lib/posts";
 import { Post } from "../components/Post";
 
-const Blog = ({ posts}) => {
+const Blog = ({ posts }) => {
     return (
         <Layout title="Blog">
             <ul className="m-10">
